535d578ffeac220c7673a967eacf703118e3f9ff
Davide <d.monsurrocco@studenti.unina.it>
2018-05-21 14:32:44 +0200

    v1

---
+++ parameters.h
@@ -0,0 +1,19 @@
+//---------------- PARAMETERS.H ----------------------- 
+
+#define TICK_TIME 100000000
+#define CNTRL_TIME 50000000
+
+#define TASK_PRIORITY 1
+
+#define STACK_SIZE 10000
+
+#define BUF_SIZE 10
+
+#define SEN_SHM 121111
+#define ACT_SHM 112112
+#define REFSENS 111213
+
+#define SPACE_SEM 1234444
+#define MEAS_SEM 1234445
+
+#define NUM_OF_WHEELS 2
